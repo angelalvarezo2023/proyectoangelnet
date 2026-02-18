39977f4d4ddc85165c3ad8ac182f7d27b1fbdf09
angelalvarezo2023 <150785070+angelalvarezo2023@users.noreply.github.com>
2026-02-18 01:16:46 -0400

    Create page.tsx

---
+++ app/angel-rent/page.tsx
@@ -0,0 +1,9 @@
+import AngelRentLogin from "@/components/angel-rent-login";
+
+export const metadata = {
+  title: "Angel Rent",
+};
+
+export default function AngelRentPage() {
+  return <AngelRentLogin />;
+}
